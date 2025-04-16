71e72e49804706f50a8a1a8484faaadaf8ded1ae
zehra <zehragrbovic02@gmail.com>
2025-04-17 00:02:52 +0200

    Video can be played

--- video-template.js
+++ video-template.js
@@ -1,10 +1,20 @@
-function videoFunction() {
+var video = document.createElement("VIDEO");
 
-    var video = document.createElement("VIDEO");
+video.setAttribute("id", "video-element");
+video.setAttribute("width", "320");
+video.setAttribute("height", "240");
+video.setAttribute("controls", "true");
 
-    video.setAttribute("width", "320");
-    video.setAttribute("height", "240");
-    video.setAttribute("controls", "true");
-    document.body.appendChild(video);
+video.src="https://storage.googleapis.com/interactive-media-ads/media/android.mp4";
 
-} 
+document.body.appendChild(video);
+
+// On window load, attach an event to the play button click
+// that triggers playback on the video element
+window.addEventListener('load', function(event) {
+    video = document.getElementById('video-element');
+    var playButton = document.getElementById('play-button');
+    playButton.addEventListener('click', function(event) {
+      videoElement.play();
+    });
+});
